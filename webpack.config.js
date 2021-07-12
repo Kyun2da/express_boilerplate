7039bb2b2e135e56f13b856a47758b4da76e4701
Kyun Heo <kyun2dot@gmail.com>
2021-07-13 00:40:11 +0900

    추가 : 웹팩 설정

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  target: "node",
+  mode: "development",
+  entry: {
+    // 어떤 파일을 진입파일로 설정할지 세팅
+    main: "./bin/www",
+  },
+  output: {
+    // 결과 파일 경로와 이름 설정
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
